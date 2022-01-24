c1cced0d5061bd06a589663a0aaceb339dcf5423
GitHub Action <action@github.com>
2022-01-24 08:48:25 +0000

    Template cleanup

--- src/main/kotlin/com/github/jbenchitrit/autorefresh/services/MyProjectService.kt
+++ src/main/kotlin/com/github/jbenchitrit/autorefresh/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.jbenchitrit.autorefresh.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.jbenchitrit.autorefresh.MyBundle
 
 class MyProjectService(project: Project) {
 
